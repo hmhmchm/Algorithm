6df63661cc992563625659263df162eccd04921b
Hyeonmin Choi <hyeonminchoi5@gmail.com>
2022-02-02 23:31:44 +0900

    Create 신고결과받기.java

---
+++ Programmers/1level/신고결과받기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        // 이용자 ID , 이용자가 신고한 ID, 정지 기준
+        // 각 유저별로 처리 결과 메일을 받은 횟수를 배열에 담아 return
+        int[] answer = {};
+        
+        return answer;
+    }
+}
