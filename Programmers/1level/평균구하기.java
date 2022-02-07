07ddda8857008854972da188aa85abd557a128a2
Hyeonmin Choi <hyeonminchoi5@gmail.com>
2022-02-08 01:03:02 +0900

    .

---
+++ Programmers/1level/평균구하기.java
@@ -0,0 +1,10 @@
+public class 평균구하기 {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        answer /= arr.length;
+        return answer;
+    }
+}
