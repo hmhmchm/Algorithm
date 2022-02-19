39a03d11002ca250db16d46bb4e99a06edf96779
hmhmchm <hyeonminchoi5@gmail.com>
2022-02-19 09:32:53 +0900

    1level

---
+++ Programmers/1level/같은숫자는싫어.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class 같은숫자는싫어 {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        ArrayList<Integer> list = new ArrayList<>();
+        int tmp = 11;
+
+        for (int i =  0;  i < arr.length; i++){
+            if(arr[i] != tmp) list.add(arr[i]);
+            tmp = arr[i];
+        }
+        answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
